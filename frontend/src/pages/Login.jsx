118de0e46c529269039e06cb7a670af4e3175c59
unknown <alan8589824041@gmail.com>
2025-07-11 18:16:56 +0530

    changed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
       
       if(state === 'Sign Up'){
         
-        const {data} = await axios.post('http://localhost:4000/api/user/register', {name, email, password})
+        const {data} = await axios.post('https://serenocare-backend.onrender.com/api/user/register', {name, email, password})
 
         if(data.success){
           localStorage.setItem('token',data.token)
@@ -31,7 +31,7 @@ const Login = () => {
 
       } else {
 
-        const {data} = await axios.post('http://localhost:4000/api/user/login', {email, password})
+        const {data} = await axios.post('https://serenocare-backend.onrender.com/api/user/login', {email, password})
 
         if(data.success){
           localStorage.setItem('token',data.token)
